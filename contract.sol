1bcf88004b485f8ac05e36695dd43d4008e669e6
Diyp <diypa571@gmail.com>
2021-07-18 13:18:59 +0200

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,16 @@
+// Forked
+pragma solidity ^0.6.6;
+
+
+contract CoolNumberContract {
+    uint public coolNumber = 10;
+    
+    
+    
+    function setCoolNumber(uint _coolNumber) public {
+        coolNumber = _coolNumber;
+    }
+    
+    
+    
+}
